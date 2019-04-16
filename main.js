70ad7d2b7744721e701dbb5acea2cca7b1735458
esperanza-b <42615963+esperanza-b@users.noreply.github.com>
2019-04-16 09:08:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var panel= $("#quiz-area")
+var countStartNumber = 30;
+
+//Question Set
+var question = [
+    
+    {
+     question: "What kind of animal was Gentle Ben on the TV show?",
+     answers:["Frog", "Tiger","Dog"],
+     correctAnswer:"A Bear",
+     image:"assets\bear.gif",
+
+     question: "A female donkey is called a what?",
+     answers:["Donkeyness", "Luna","Tigress"],
+     correctAnswer:"A Jenny",
+     image:"assets\donkey-gif-.gif",
+     
+     
+     
+    }
+
+]
