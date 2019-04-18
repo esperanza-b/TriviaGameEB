a07ee90e1c82dcae7d877b818dfa765f395eb572
esperanza-b <42615963+esperanza-b@users.noreply.github.com>
2019-04-18 08:55:05 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var panel= $("#quiz-area")
+ var panel= $("#quiz-area")
 var countStartNumber = 30;
 
 //Question Set
@@ -8,15 +8,64 @@ var question = [
      question: "What kind of animal was Gentle Ben on the TV show?",
      answers:["Frog", "Tiger","Dog"],
      correctAnswer:"A Bear",
-     image:"assets\bear.gif",
+     image:"Assets2\bear.gif",
+    },
 
-     question: "A female donkey is called a what?",
+     {
+         question: "A female donkey is called a what?",
      answers:["Donkeyness", "Luna","Tigress"],
      correctAnswer:"A Jenny",
-     image:"assets\donkey-gif-.gif",
-     
-     
-     
-    }
+     image:"Assets2\donkey.gif",
+      
+    },
 
 ]
+//Variable to hold out setInternal
+var timer;
+var game = {
+
+    questions: question, 
+    correctQuestion: 0,
+    counter: countStartNumber,
+    correct: 0, 
+    incorrect: 0,
+
+    countdown: function() {
+        game.counter--;
+        $("#counter-number").html(game.counter);
+        if(game.counter=== 0){
+            console.log("TIME'S UP!");
+            game.timeUp();
+
+        }
+        
+    },
+
+     loadQuestion: function() 
+     {
+         timer = setInterval(game.countdown, 1000);
+         panel.html("<h2>" + question[this.incorrect.currentQuestion].question + '</h2>');
+         for(var i = 0; i< questions[this.currentQuestion].answers.length; i++);
+        }
+       
+        
+        panel.append("<button class= 'answer-button' id= 'button' data-name=' " + 
+        questions[this.currentQuestion].answer[i] +"'>"+ questions[this.currentQuestion.answer[i]
+        + "</buttons>"]
+        )
+
+    };
+         
+            
+
+
+         nextQuestion: function(){
+             game.counter = countStartNumber;
+             $("#counter-number").html(game.counter);
+             game.currentQuestion++;
+             game.loadQuestion9;
+         }
+          timeUp: function() {
+
+          }
+        };
