c7d4ccac50b95e3a14201e937fbd3aa9d98f8e98
Barrios <0335054@students.dadeschools.net>
2019-04-23 11:41:45 -0400

    finish Timeout

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ var question = [
      question: "What kind of animal was Gentle Ben on the TV show?",
      answers:["Frog", "Tiger","Dog"],
      correctAnswer:"A Bear",
-     image:"Assets2\bear.gif",
+     image:"Assets/bear.gif",
     },
 
      {
          question: "A female donkey is called a what?",
      answers:["Donkeyness", "Luna","Tigress"],
      correctAnswer:"A Jenny",
-     image:"Assets2\donkey.gif",
+     image:"Assets/donkey.gif",
       
     },
 
@@ -51,21 +51,47 @@ var game = {
         
         panel.append("<button class= 'answer-button' id= 'button' data-name=' " + 
         questions[this.currentQuestion].answer[i] +"'>"+ questions[this.currentQuestion.answer[i]
-        + "</buttons>"]
-        )
+        + "</buttons>"])
 
     };
          
             
 
 
-         nextQuestion: function(){
+         nextQuestion: function()
+         {
              game.counter = countStartNumber;
              $("#counter-number").html(game.counter);
              game.currentQuestion++;
              game.loadQuestion9;
          }
           timeUp: function() {
+              clearInterval(timer);
 
-          }
+              $("counter-number").html(game.counter);
+
+              panel.html("<h2>Out of Time</h2>");
+              panel.append("<h3>The correct answer was: </h3>"+ questions[this.currentQuestion].correctAnswer);
+              panel.append("<img src'"+ questions[this.currentQuestion].image);
+
+              if (game.currentQuestion === questions.length -1)
+              {
+                 setTimeout(game.results, 3 * 1000); 
+              }
+              else{
+                  setTimeout(game.realQuestion, 3 * 1000);
+              }
+            
+              results: function(){
+
+                clearInterval(timer);
+                panel.html("<h2>All done, here's how you did</h2>");
+                $("counter-number").html(game.counter);
+                panel.append("<h3>Correct Answers;" + game.correct + "</h3>");
+                panel.append("<h3>Incorrect Answers;"+ game.incorrect + "</h3>");
+                panel.append("<h3>Unanswered;" +(questions.length - (game.correct + game.incorrect))+ "</h3>");
+                panel.append("<br><button id = 'start over'>Start Over?</button>")
+
+              }
+           }
         };
